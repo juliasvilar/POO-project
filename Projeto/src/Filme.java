0ac07920bb3e19d285a155f9eecc9d9023504e54
Júlia Saldanha <juliasvilar@gmail.com>
2024-05-15 21:39:41 -0300

    Update Filme.java
    adição da função de adicionar filme à lista de filmes

--- Projeto/src/Filme.java
+++ Projeto/src/Filme.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class Filme {
     
     List<Filme> listaDeFilmes = new ArrayList<>();
-    
     private String nome;
     private double nota;
         
@@ -33,5 +33,42 @@ class Filme {
     public List<Filme> getFilmes() {
         return listaDeFilmes;
     }
-}
+    
+    public static void adicionarFilme(Scanner scanner, List<Filme> ListaDeFilmes, Favoritos favoritos){
+        scanner.nextLine(); 
+        Filme filme = new Filme();
+        System.out.print("Digite o nome do filme: ");
+        String nomeFilme = scanner.nextLine();
 
+        boolean achou = false;
+        for (Filme f : ListaDeFilmes) {
+            if (f.getNome().equalsIgnoreCase(nomeFilme)) {
+                achou = true;
+                break;
+            }
+        }
+
+        if (achou == true) {
+            System.out.println("Este filme ja esta na lista!");
+        } else {
+            filme.setNome(nomeFilme);
+
+            System.out.print("Digite a nota para o filme '" + filme.getNome() + "': ");
+            double notaFilme = scanner.nextDouble();
+            filme.setNota(notaFilme);
+
+            ListaDeFilmes.add(filme);
+            System.out.println("Deseja adicionar o filme na lista dos favoritos? ");
+            System.out.println("1 - SIM\n2 - NAO: ");
+            int fav = scanner.nextInt();
+            while(fav != 1 && fav != 2){
+                System.out.println("Resposta inválida!\n");
+            }
+            if(fav == 1){
+                favoritos.adicionarFilme(filme);
+                System.out.println("Filme adicionado aos favoritos!");
+
+            }
+        }
+    }   
+}
