4309088c13fc47f3c9b1a99a073f0a071e689b69
Júlia Saldanha <juliasvilar@gmail.com>
2024-06-01 15:41:02 -0300

    função remover filme favorito
    criação de uma função que recebe o nome de um filme da lista de favoritos e o remove dela (caso exista)

--- Projeto/src/Favoritos.java
+++ Projeto/src/Favoritos.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class Favoritos {
     public List<Filme> filmesFavoritos;
@@ -25,5 +26,26 @@ class Favoritos {
     public List<Livro> getLivrosFavoritos() {
         return livrosFavoritos;
     }
+    
+    
+    public void removerFilmeFavorito() {
+        if (getFilmesFavoritos().isEmpty()) {
+            System.out.println("Lista vazia!");
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Que filme você deseja remover da lista de favoritos? ");
+            String filme = scanner.nextLine();
 
+            for(int i = 0; i < filmesFavoritos.size(); i++){
+                Filme p = filmesFavoritos.get(i);
+
+                if(p.getNome().equals(filme)){
+                    filmesFavoritos.remove(p);
+                    System.out.println("Filme removido da lista de favoritos!");
+                    return;
+                }
+            }
+            System.out.println("Filme nao favoritado.\n");
+        }
+    }
 }
