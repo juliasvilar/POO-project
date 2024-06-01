5348b3ea7ae38fcfc3c195b1bc2a5374edbed499
Júlia Saldanha <juliasvilar@gmail.com>
2024-06-01 20:39:39 -0300

    Create Perfil.java
    criação da classe Perfil para manipulação de informações do usuário quando for feito o cadastro, login ou alteração dos seus dados

---
+++ Projeto/src/Perfil.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class Perfil {
+        
+    String nome = "";
+    String email = "";
+    String senha = "";
+    
+    public void edicao(){
+    
+        System.out.println("O que voce deseja mudar?\n1 - Email\n2 - Nome de usuario\n3 - Senha\nEscolha: ");
+        Scanner scanner = new Scanner(System.in);
+        int resposta;
+        
+        resposta = scanner.nextInt();
+        while(resposta < 1 || resposta > 3){
+            System.out.println("Escolha invalida. Tente de novo: ");
+            resposta = scanner.nextInt();
+        }
+        
+        if (resposta == 1){
+            String emailAntigo = email;
+            System.out.println("Qual será o seu novo email? ");
+            email = scanner.nextLine();
+            while (emailAntigo.equals(email)){
+                System.out.println("Escolha um email diferente do atual: ");
+                email = scanner.nextLine();
+            }
+            System.out.println("Email alterado com sucesso!\n");
+        
+        } else if (resposta == 2){
+            String nomeAntigo = nome;
+            System.out.println("Qual será o seu novo nome de usuario? ");
+            nome = scanner.nextLine();
+            while (nomeAntigo.equals(nome)){
+                System.out.println("Escolha um nome de usuario diferente do atual: ");
+                nome = scanner.nextLine();
+            }
+            System.out.println("Nome de usuario alterado com sucesso!\n");
+        
+        } else {
+            String senhaAntiga = senha;
+            System.out.println("Qual será a sua nova senha? ");
+            senha = scanner.nextLine();
+            while (senhaAntiga.equals(senha)){
+                System.out.println("Escolha uma senha diferente da atual: ");
+                senha = scanner.nextLine();
+            }
+            System.out.println("Senha alterada com sucesso!\n");
+        }        
+    }
+}
